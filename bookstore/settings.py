add93ee1504fadd85225e0b0cdb5932b8c62e9b9
ajinkyaimpossible <ajinkyaimpossible@gmail.com>
2017-05-09 01:51:29 +0530

    Add Review Form And saving

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'social.apps.django_app.context_processors.backends',
                 'social.apps.django_app.context_processors.login_redirect',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media',
             ],
         },
     },
